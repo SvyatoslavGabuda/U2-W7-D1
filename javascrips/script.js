65f97715c08566eda5633b8b31b9cc9cda0950cd
SvyatoslavGabuda <sgabuda@gmail.com>
2023-01-23 15:51:03 +0100

    a

--- javascrips/script.js
+++ javascrips/script.js
@@ -20,37 +20,57 @@ const persona1 = new Persona("Mario", "Rossi", "1900-01-01");
 const persona2 = new Persona("Luigi", "Bianchi", "1900-02-01");
 const persona3 = new Persona("Carlo", "Verdi", "1900-03-01");
 
+Persona.prototype.nReggistrazione = "";
+
+persona1.nReggistrazione = 1;
+persona2.nReggistrazione = 2;
+persona3.nReggistrazione = 3;
+
 const arrayPersone = [persona1, persona2, persona3];
+let NumeroReggistrazione = arrayPersone.length;
 arrayPersone.forEach((pers) => {
   const riga = document.createElement("tr");
+  const colNumero = document.createElement("td");
   const colNome = document.createElement("td");
   const colCognome = document.createElement("td");
   const colData = document.createElement("td");
+  colNumero.innerText = pers.nReggistrazione;
 
   colNome.innerText = pers.nome;
   colCognome.innerText = pers.cognome;
   colData.innerText = pers.dataDiNascita;
+  riga.appendChild(colNumero);
   riga.appendChild(colNome);
   riga.appendChild(colCognome);
   riga.appendChild(colData);
   tbody.appendChild(riga);
 });
+
 console.log(arrayPersone);
+
 const aggiungiPersona = function () {
   const riga = document.createElement("tr");
+  const colNumero = document.createElement("td");
   const colNome = document.createElement("td");
   const colCognome = document.createElement("td");
   const colData = document.createElement("td");
+
   const personaN = new Persona(
     nameInput.value,
     cognomeInput.value,
     dataInput.value
   );
+  personaN.nReggistrazione = NumeroReggistrazione + 1;
+  NumeroReggistrazione++;
+
   arrayPersone.push(personaN);
 
+  colNumero.innerText = personaN.nReggistrazione;
   colNome.innerText = personaN.nome;
   colCognome.innerText = personaN.cognome;
   colData.innerText = personaN.dataDiNascita;
+
+  riga.appendChild(colNumero);
   riga.appendChild(colNome);
   riga.appendChild(colCognome);
   riga.appendChild(colData);
